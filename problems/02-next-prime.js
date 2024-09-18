2466151fa3fd02b6624f641dd44bf0aeae4a1e87
yaseenahmad1 <yahmad09@gmail.com>
2024-09-18 14:16:25 -0700

    problem 2 solved

--- problems/02-next-prime.js
+++ problems/02-next-prime.js
@@ -7,14 +7,41 @@ function should return the nearest prime number that is greater than the given
 number.
 */
 
-// Your code here 
+let isPrime = function(number) {
+  if (number < 2) { //essentially this is saying that if num is 1 or 0 it is not a prime so return a false boolean. 
+      return false; 
+  }
 
-// console.log(nextPrime(2)); // 3
-// console.log(nextPrime(3)); // 5
-// console.log(nextPrime(7)); // 11
-// console.log(nextPrime(8)); // 11
-// console.log(nextPrime(20)); // 23
-// console.log(nextPrime(97)); // 101
+  for (let i = 2; i < number; i++) { //start i at 2 because 1 or 0 is not a prime and then increment but why are we incrementing? 
+      if (number % i ===  0) {
+          return false; 
+      }
+  }
+  return true; 
+}
+
+
+//this code has to take in any number and return the nearest prime number that is greater than the number given. 
+//if we start to iterate in a loop starting with the number given and end it once it reaches a prime number 
+//then we can have it return that prime using an if statement. 
+//maybe assign a variable that will return 
+let nextPrime = function(number) {
+
+  let next = number + 1; 
+
+  while (!(isPrime(next))) { //While next is not a prime number
+    next++;                  //Increment next by 1; 
+  }
+
+  return next;              //Once you find the prime, return it
+}
+
+console.log(nextPrime(2)); // 3
+console.log(nextPrime(3)); // 5
+console.log(nextPrime(7)); // 11
+console.log(nextPrime(8)); // 11
+console.log(nextPrime(20)); // 23
+console.log(nextPrime(97)); // 101
 
 /******************** DO NOT MODIFY ANY CODE BELOW THIS LINE *****************/
 module.exports = {
